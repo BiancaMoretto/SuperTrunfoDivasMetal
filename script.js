037e5c084a6ff6f8424f00f76e789931315af7ed
BIANCA ALVES MORETTO <102807406+BiancaMoretto@users.noreply.github.com>
2022-09-26 19:27:42 -0400

    commit inicial do projeto

---
+++ script.js
@@ -0,0 +1,411 @@
+let carta1 = {
+  nome: "Epica",
+  imagem: "https://www.vagalume.com.br/epica/images/epica.jpg",
+  atributos: { vocal: 7, presenca: 8, visual: 6.5 },
+};
+
+let carta2 = {
+  nome: "Sharon Den Adel - Within Temptation",
+  imagem:
+    "https://www.netrock.com.br/content/uploads/photos/2021/07/netrock_b552c62150b14a260b6e46aac426dfe1.jpg",
+  atributos: { vocal: 8.5, presenca: 9, visual: 9 },
+};
+
+let carta3 = {
+  nome: "Tarja",
+  imagem:
+    "https://mir-s3-cdn-cf.behance.net/project_modules/fs/649e5570461721.5ba3f21a6f40c.jpg",
+  atributos: { vocal: 9, presenca: 9, visual: 9 },
+};
+
+let carta4 = {
+  nome: "Dianne Van Giersbergen",
+  imagem:
+    "https://i.pinimg.com/236x/2b/e6/8c/2be68cd629aacc0d19377990f04e05da.jpg",
+  atributos: { vocal: 8, presenca: 7, visual: 6 },
+};
+
+let carta5 = {
+  nome: "Trail of Tears",
+  imagem:
+    "http://www.metalcallout.com/sites/default/files/wiki/trail-of-tears.jpg",
+  atributos: { vocal: 8, presenca: 7, visual: 7 },
+};
+
+let carta6 = {
+  nome: "Evanescence",
+  imagem:
+    "http://pm1.narvii.com/6851/09575512317240066647ff5a05d2e3913d4647f4v2_00.jpg",
+  atributos: { vocal: 9, presenca: 9, visual: 8 },
+};
+
+let carta7 = {
+  nome: "Evanescence",
+  imagem:
+    "http://pm1.narvii.com/6851/09575512317240066647ff5a05d2e3913d4647f4v2_00.jpg",
+  atributos: { vocal: 9, presenca: 9, visual: 8 },
+};
+
+let carta8 = {
+  nome: "Floor Jansen",
+  imagem:
+    "https://netherlands.postsen.com/content/uploads/2022/06/18/2776a139b0.jpg",
+  atributos: { vocal: 9.5, presenca: 9.5, visual: 8.5 },
+};
+
+let carta9 = {
+  nome: "Liv Kristine",
+  imagem: "https://www.metal-archives.com/images/1/7/4/7/1747_artist.jpg?4921",
+  atributos: { vocal: 9.5, presenca: 8, visual: 9 },
+};
+
+let carta10 = {
+  nome: "Amaranthe",
+  imagem:
+    "https://i.pinimg.com/736x/fc/18/d0/fc18d07a0b99b532ff741f29960898d2.jpg",
+  atributos: { vocal: 8, presenca: 10, visual: 8 },
+};
+
+let carta11 = {
+  nome: "Anneke Van Giersbergen",
+  imagem:
+    "https://i.pinimg.com/originals/8f/b9/00/8fb90069d63ca8c3f4c485d566fdab4e.jpg",
+  atributos: { vocal: 10, presenca: 9, visual: 7 },
+};
+
+let carta12 = {
+  nome: "Anette Olzon",
+  imagem:
+    "https://i.discogs.com/Om8H879ZVgGBqkPcF203aFvDrpVVmpuY7xGOCG0RAa4/rs:fit/g:sm/q:90/h:773/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTk0NjI1/Ni0xNjYyMzYzMzkz/LTEyNzQuanBlZw.jpeg",
+  atributos: { vocal: 7.5, presenca: 8.5, visual: 7 },
+};
+
+let carta13 = {
+  nome: "Heidi Parviainen",
+  imagem:
+    "https://i.pinimg.com/736x/0d/a7/ec/0da7ecf82a424d78a5d9669a01b61dcf--dawn-goddesses.jpg",
+  atributos: { vocal: 8.5, presenca: 7, visual: 8 },
+};
+
+let carta14 = {
+  nome: "Heike Langhans",
+  imagem:
+    "https://i.discogs.com/sSK8sgwe7VenWHLra_sd7hIFxBaUVk2UnuYaWlQIPXo/rs:fit/g:sm/q:90/h:800/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTQ0NTA4/NTktMTQ0NjM3NDkz/Mi0yNDc3LmpwZWc.jpeg",
+  atributos: { vocal: 8, presenca: 7, visual: 8 },
+};
+
+let carta15 = {
+  nome: "Crypta",
+  imagem:
+    "https://sonoridadeunderground.com.br/wp-content/uploads/2022/05/crypta-promo-2022-02.jpg",
+  atributos: { vocal: 7.5, presenca: 8, visual: 7.5 },
+};
+
+let carta16 = {
+  nome: "Marcela Bovio",
+  imagem:
+    "https://darkviolin.com/wp-content/uploads/2014/09/marcela-bovio-credit-richelle-IJland-555px-1962863_10152108050238681_1741832730_n.jpg",
+  atributos: { vocal: 9.5, presenca: 7, visual: 8 },
+};
+
+let carta17 = {
+  nome: "Cristina Scabbia",
+  imagem:
+    "https://www.rafabasa.com/wp-content/uploads/imagenes/web/lacuna_coil/cristina2020_1.jpg",
+  atributos: { vocal: 8, presenca: 9.5, visual: 9 },
+};
+
+let carta18 = {
+  nome: "Vibeke Stene",
+  imagem:
+    "https://i.pinimg.com/236x/16/2d/2c/162d2cc7ea01588ad6a1c479c40c630d--female-singers-symphonic-metal.jpg",
+  atributos: { vocal: 9.5, presenca: 9.5, visual: 9 },
+};
+
+let carta19 = {
+  nome: "Lzzy Hale",
+  imagem:
+    "https://i.pinimg.com/originals/16/e6/fc/16e6fc9c71fc6f116d4c06284d0a1962.jpg",
+  atributos: { vocal: 8, presenca: 9, visual: 7 },
+};
+
+let carta20 = {
+  nome: "Marina La Torraca",
+  imagem: "https://pbs.twimg.com/media/EaKOvzEUYAEKfPH.jpg",
+  atributos: { vocal: 8, presenca: 7, visual: 7 },
+};
+
+let carta21 = {
+  nome: "Mary Demurtas",
+  imagem:
+    "https://princesasdorock.files.wordpress.com/2011/07/tristania-7112.jpg",
+  atributos: { vocal: 8, presenca: 8, visual: 7 },
+};
+
+let carta22 = {
+  nome: "Charlotte Wessels",
+  imagem: "https://fstvls.s3.amazonaws.com/97/97470.jpeg",
+  atributos: { vocal: 8.5, presenca: 7, visual: 8 },
+};
+
+let carta23 = {
+  nome: "Amanda Sommervile",
+  imagem: "https://iv1.lisimg.com/image/8792289/715full-amanda-somerville.jpg",
+  atributos: { vocal: 9, presenca: 7, visual: 7 },
+};
+
+let carta24 = {
+  nome: "Alyssa White Gluzz",
+  imagem:
+    "https://www.netrock.com.br/content/uploads/photos/2021/07/netrock_f19c99702513faad79ddb20dc8df12b6.jpg",
+  atributos: { vocal: 8, presenca: 9, visual: 9 },
+};
+
+let carta25 = {
+  nome: "Spiritbox",
+  imagem:
+    "https://i0.wp.com/glassefactory.com/wp-content/uploads/2021/10/Spiritbox-full-band.jpeg",
+  atributos: { vocal: 9.5, presenca: 9, visual: 8.5 },
+};
+
+let carta26 = {
+  nome: "Angélica Burns -hATEFULMURDER",
+  imagem:
+    "https://i.pinimg.com/originals/75/13/62/751362c94d68b003b17174ff984ecef8.jpg",
+  atributos: { vocal: 8.5, presenca: 9, visual: 5.5 },
+};
+
+let carta27 = {
+  nome: "Dani Nolden- Shadowside",
+  imagem:
+    "http://s2.glbimg.com/OREHjJJ40fqpJrzWQq6pV0egSReZ7Ipacx7gpMnMtC1Ioz-HdGixxa_8qOZvMp3w/s.glbimg.com/jo/g1/f/original/2012/05/10/shadowside_daninolden_estudio.jpg",
+  atributos: { vocal: 7.5, presenca: 8.5, visual: 7.5 },
+};
+
+let carta28 = {
+  nome: "Clementine Delauney",
+  imagem:
+    "https://iv1.lisimg.com/image/22142824/736full-cl%C3%A9mentine-delauney-photo.jpg",
+  atributos: { vocal: 8.5, presenca: 8, visual: 8.5 },
+};
+
+let carta29 = {
+  nome: "Silent Cry",
+  imagem:
+    "https://akamai.sscdn.co/uploadfile/letras/fotos/1/e/2/8/1e280c0c9ab40518dff3f926973c7cbd.jpg",
+  atributos: { vocal: 10, presenca: 9, visual: 10 },
+};
+
+let carta30 = {
+  nome: "Vivs Takahashi - Flowerleaf",
+  imagem:
+    "https://i.pinimg.com/474x/a4/c0/75/a4c075b594e2135423e4f2544787e8da.jpg",
+  atributos: { vocal: 9, presenca: 9, visual: 8 },
+};
+
+let carta31 = {
+  nome: "Sandra Schleret - Eyes of Eden/Dreams of sanity/Elis",
+  imagem:
+    "https://i.discogs.com/aQQEeBDVmJNp_abegZrsq9uhBIh4mHmHUUs1QYvfFSU/rs:fit/g:sm/q:90/h:803/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTEwNjQy/MTktMTQxMjI4Njkz/OS03NDM3LmpwZWc.jpeg",
+  atributos: { vocal: 8.5, presenca: 7.5, visual: 8.5 },
+};
+
+let carta32 = {
+  nome: "Doro Pesch",
+  imagem:
+    "http://pm1.narvii.com/6387/5bbdc6b728202132b0bc9d8e868c745f14012ce5_00.jpg",
+  atributos: { vocal: 9.5, presenca: 9, visual: 10 },
+};
+
+let cartaMaquina;
+let cartaJogador;
+
+const cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23,
+  carta24,
+  carta25,
+  carta26,
+  carta27,
+  carta28,
+  carta29,
+  carta30,
+  carta31,
+  carta32,
+];
+
+var pontosMaquina = 0;
+var pontosJogador = 0;
+document.getElementById("btnRodada").disabled = true;
+
+function sortearCarta() {
+  var numCartaMaquina = parseInt(Math.random() * 32);
+  cartaMaquina = cartas[numCartaMaquina];
+  var numCartaJogador = parseInt(Math.random() * 32);
+
+  while (numCartaMaquina == numCartaJogador) {
+    numCartaJogador = parseInt(Math.random() * 32);
+  }
+  cartaJogador = cartas[numCartaJogador];
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  mostrarCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var resultadoFinalHTML = document.getElementById("resultado");
+  var pontuacaoFinalJogador = document.getElementById("pJogador");
+  var pontuacaoFinalMaquina = document.getElementById("pMaquina");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    pontosJogador++;
+    htmlResultado =
+      "<p class='resultado-final'>VOCÊ VENCEU ESSA RODADA!!! CONTINUE ASSIM!!!</p>";
+    pontuacaoJogadorHTML = `<p class='ponto-final-jogador'>Pontos Jogador: ${pontosJogador}</p>`;
+    pontuacaoMaquinaHTML = `<p class='ponto-final-maquina'>Pontos Maquina: ${pontosMaquina}</p>`;
+    document.getElementById("btnRodada").disabled = false;
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    pontosMaquina++;
+    htmlResultado =
+      "<p class='resultado-final'>VOCÊ PERDEU ESSA RODADA!!! NÃO DESISTA!!!</p>";
+    pontuacaoJogadorHTML = `<p class='ponto-final-jogador'>Pontos Jogador: ${pontosJogador}</p>`;
+    pontuacaoMaquinaHTML = `<p class='ponto-final-maquina'>Pontos Maquina: ${pontosMaquina}</p>`;
+    document.getElementById("btnRodada").disabled = false;
+  } else {
+    pontuacaoJogadorHTML = `<p class='ponto-final-jogador'>Pontos Jogador: ${pontosJogador}</p>`;
+    pontuacaoMaquinaHTML = `<p class='ponto-final-maquina'>Pontos Maquina: ${pontosMaquina}</p>`;
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+    document.getElementById("btnRodada").disabled = false;
+  }
+  resultadoFinalHTML.innerHTML = htmlResultado;
+  pontuacaoFinalJogador.innerHTML = pontuacaoJogadorHTML;
+  pontuacaoFinalMaquina.innerHTML = pontuacaoMaquinaHTML;
+  document.getElementById("btnJogar").disabled = true;
+  mostrarCartaMaquina();
+}
+
+function mostrarCartaJogador() {
+  var htmlCartaJogador = document.getElementById("carta-jogador");
+  htmlCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  htmlCartaJogador.innerHTML =
+    moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function mostrarCartaMaquina() {
+  var htmlCartaMaquina = document.getElementById("carta-maquina");
+  htmlCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  htmlCartaMaquina.innerHTML =
+    moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function proximaRodada() {
+  var novoJogoHTML = document.getElementById("novo-jogo");
+  var resultadoFinalHTML2 = document.getElementById("resultado");
+  var musica = document.getElementById("musica");
+
+  if (pontosJogador == 5) {
+    htmlResultado = `<p class='resultado-final'>Você Venceu, o Placar foi Jogador: ${pontosJogador} Máquina: ${pontosMaquina}</p>`;
+    botaoJogo = `<button type="button" onclick="novoJogo()" id="btnNovoJogo">Novo Jogo</button>`;
+    musicaHTML = `<audio autoplay="autoplay"><source src="https://www.soundsnap.com/crowd_national_theater_hnk_ivan_zjec_crowd_and_people_after_the_performance_rijeka_city_croatia" type="audio/mp3" /></audio> `;
+    document.getElementById("btnRodada").disabled = true;
+    musica.innerHTML = musicaHTML;
+    resultadoFinalHTML2.innerHTML = htmlResultado;
+    novoJogoHTML.innerHTML = botaoJogo;
+  } else if (pontosMaquina == 5) {
+    htmlResultado = `<p class='resultado-final'>Você Perdeu, o Placar foi Máquina: ${pontosMaquina} Jogador: ${pontosJogador}</p>`;
+    botaoJogo = `<button type="button" onclick="novoJogo()" id="btnNovoJogo">Novo Jogo</button>`;
+    document.getElementById("btnRodada").disabled = true;
+    resultadoFinalHTML2.innerHTML = htmlResultado;
+    novoJogoHTML.innerHTML = botaoJogo;
+  } else {
+    document.getElementById("btnRodada").disabled = true;
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById("carta-jogador").innerHTML = "";
+    document.getElementById("carta-jogador").style.backgroundImage = "";
+    document.getElementById("carta-maquina").innerHTML = "";
+    document.getElementById("carta-maquina").style.backgroundImage = "";
+    sortearCarta();
+  }
+}
+function novoJogo() {
+  document.getElementById("musica").innerHTML = "";
+  document.getElementById("carta-jogador").innerHTML = "";
+  document.getElementById("carta-jogador").style.backgroundImage = "";
+  document.getElementById("carta-maquina").innerHTML = "";
+  document.getElementById("carta-maquina").style.backgroundImage = "";
+  document.getElementById("resultado").innerHTML = "";
+  document.getElementById("pJogador").innerHTML = "";
+  document.getElementById("pMaquina").innerHTML = "";
+  pontosMaquina = 0;
+  pontosJogador = 0;
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("novo-jogo").innerHTML = "";
+}
